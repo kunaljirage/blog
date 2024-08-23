adbb74e58c761f66ecb7bee059e223725363ff67
kunal jirage <kunalsjirage@gmail.com>
2024-08-23 09:05:58 +0530

    Fixed the css for add post page

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -6,56 +6,68 @@ const Write = () => {
   const [value, setValue] = useState('');
 
   return (
-    <div className="add">
-      <div className="content">
-        <input type="text" placeholder="Title" />
-        <div className="editorContainer">
-          <ReactQuill theme="snow" value={value} onChange={setValue} />
-        </div>
-      </div>
-      <div className="menu">
-        <div className="item">
-          <h1>Publish </h1>
-          <span>
-            <b>Status: </b> Draft
-          </span>
-          <span>
-            <b>Visibility:</b> Public
-          </span>
-          <input type="file" name="" id="file" style={{ display: 'none' }} />
-          <label htmlFor="file">upload Image </label>
-          <div className="buttons">
-            <button type="button">Save as a draft</button>
-            <button type="button"> update</button>
+    <div className="add-post">
+      <div className="add">
+        <div className="content">
+          <input type="text" placeholder="Title" />
+          <div className="editorContainer">
+            <ReactQuill
+              className="editor"
+              theme="snow"
+              value={value}
+              onChange={setValue}
+            />
           </div>
         </div>
-        <div className="item">
-          <h1>Category</h1>
+        <div className="menu">
+          <div className="item">
+            <h1>Publish </h1>
+            <span>
+              <b>Status: </b> Draft
+            </span>
+            <span>
+              <b>Visibility:</b> Public
+            </span>
+            <input type="file" name="" id="file" style={{ display: 'none' }} />
+            <label htmlFor="file">upload Image </label>
+            <div className="buttons">
+              <button type="button">Save as a draft</button>
+              <button type="button"> update</button>
+            </div>
+          </div>
+          <div className="item">
+            <h1>Category</h1>
 
-          <div className="cat">
-            <input type="radio" name="cate" value="art" id="art" />
-            <label htmlFor="art">Art</label>
-          </div>
+            <div className="cat">
+              <input type="radio" name="cate" value="art" id="art" />
+              <label htmlFor="art">Art</label>
+            </div>
 
-          <div className="cat">
-            <input type="radio" name="cate" value="science" id="science" />
-            <label htmlFor="science">Science</label>
-          </div>
-          <div className="cat">
-            <input type="radio" name="cat" value="technology" id="technology" />
-            <label htmlFor="technology">Technology</label>
-          </div>
-          <div className="cat">
-            <input type="radio" name="cate" value="cinema" id="cinema" />
-            <label htmlFor="cinema">Cinema</label>
-          </div>
-          <div className="cat">
-            <input type="radio" name="cate" value="design" id="design" />
-            <label htmlFor="design">Design</label>
-          </div>
-          <div className="cat">
-            <input type="radio" name="cate" value="food" id="food" />
-            <label htmlFor="food">Food</label>
+            <div className="cat">
+              <input type="radio" name="cate" value="science" id="science" />
+              <label htmlFor="science">Science</label>
+            </div>
+            <div className="cat">
+              <input
+                type="radio"
+                name="cat"
+                value="technology"
+                id="technology"
+              />
+              <label htmlFor="technology">Technology</label>
+            </div>
+            <div className="cat">
+              <input type="radio" name="cate" value="cinema" id="cinema" />
+              <label htmlFor="cinema">Cinema</label>
+            </div>
+            <div className="cat">
+              <input type="radio" name="cate" value="design" id="design" />
+              <label htmlFor="design">Design</label>
+            </div>
+            <div className="cat">
+              <input type="radio" name="cate" value="food" id="food" />
+              <label htmlFor="food">Food</label>
+            </div>
           </div>
         </div>
       </div>
